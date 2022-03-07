9aea8bdbe0aa5c15aac88f5adf41a64e12ce32c9
lisy09 <lisy09.thu@gmail.com>
2022-03-07 23:16:16 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# dagster-ml
+
+dagster ml extension.
+
+## Prerequisite && Setup Development Environment
+
+Check [README in dagster-ml-dev repo](../dagster-ml-dev/README.md).
---
+++ test/test_test_dagster.py
@@ -0,0 +1,16 @@
+from .test_dagster import find_highest_calorie_cereal, diamond
+
+
+def test_find_highest_calorie_cereal():
+    cereals = [
+        {"name": "hi-cal cereal", "calories": 400},
+        {"name": "lo-cal cereal", "calories": 50},
+    ]
+    result = find_highest_calorie_cereal(cereals)
+    assert result == "hi-cal cereal"
+
+
+def test_diamond():
+    res = diamond.execute_in_process()
+    assert res.success
+    assert res.output_for_node("find_highest_protein_cereal") == "Special K"
